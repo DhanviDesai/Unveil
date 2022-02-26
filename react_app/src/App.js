e7390d886903b53d4269fd6754f4bf98d22a934f
Dhanvi Desai <dhanvidesai99@gmail.com>
2022-02-27 00:24:50 +0530

    Main content basic layout added;user-short layout added

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <div className="main-content">
-        <div className="users-list"></div>
-        <div className="repos-list"></div>
+      <div className='main-content'>
+        <MainContent />
+        <MainContent />
+        <MainContent />
+        <MainContent />
+        <MainContent />
       </div>
+           
     </div>
   );
 }
