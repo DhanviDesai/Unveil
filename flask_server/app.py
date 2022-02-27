4d27b581fe020511e11589ca1b7f079da322fce0
Dhanvi Desai <dhanvidesai99@gmail.com>
2022-02-28 00:02:52 +0530

    heroku deployment changes

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
-
+from flask import Flask, send_from_directory
+from flask_cors import CORS,cross_origin
 from flask_server.routes import UNVEIL_BLUEPRINT
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="../react_app/build",static_url_path="")
+CORS(app, support_credentials=True)
 
 app.register_blueprint(UNVEIL_BLUEPRINT)
+
+@app.route('/')
+@cross_origin(supports_credentials=True)
+def serve():
+    return send_from_directory(app.static_folder,"index.html")
